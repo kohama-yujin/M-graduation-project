5311189fdfab40bf9f3ebc37b6bbdaec9fa9ad22
kinoco-yama <kinoyama001@gmail.com>
2025-06-20 15:27:34 +0900

    フォルダ変更

--- ConnectMQ3andOvrUDPTest/camera_checker.py
+++ ConnectMQ3andOvrUDPTest/camera_checker.py
@@ -1,13 +1,15 @@
 import cv2
 
-cap = cv2.VideoCapture(0)  # Ovrvision Proがデバイス1にある場合
+cap = cv2.VideoCapture(1)  # Ovrvision Proがデバイス1にある場合
 
 while True:
     ret, frame = cap.read()
     if not ret:
+        print("カメラからのフレーム取得に失敗しました。")
         break
     cv2.imshow("Ovrvision Camera", frame)
     if cv2.waitKey(1) == 27:  # ESCキーで終了
+        print("カメラチェックを終了します。")
         break
 
 print(frame.dtype, frame.shape)
