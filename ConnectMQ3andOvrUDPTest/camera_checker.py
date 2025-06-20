ac1d5da12a61c67274f6473fb4eb91ac01d33d89
kinoco-yama <kinoyama001@gmail.com>
2025-06-20 17:07:48 +0900

    画像削除、Bayer変換

--- ConnectMQ3andOvrUDPTest/camera_checker.py
+++ ConnectMQ3andOvrUDPTest/camera_checker.py
@@ -1,13 +1,28 @@
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture(1)  # Ovrvision Proがデバイス1にある場合
+cap = cv2.VideoCapture(1)
+# 解像度設定
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("カメラからのフレーム取得に失敗しました。")
         break
-    cv2.imshow("Ovrvision Camera", frame)
+
+    # VideoCaptureは8bitが限界
+    # 本来の16bitを8bitに圧縮して取得（重なる）
+    # 上位8bit：左目のグレイスケール
+    # 下位8bit：右目のBayer
+    unit8 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Bayer補間でカラー化
+    color_right = cv2.cvtColor(unit8, cv2.COLOR_BAYER_GB2BGR)
+
+    cv2.imshow("Right Eye - Color Only", color_right)
+
+    # cv2.imshow("Ovrvision Camera", frame)
     if cv2.waitKey(1) == 27:  # ESCキーで終了
         print("カメラチェックを終了します。")
         break
