0bc73c6eba6940f5e72dc44ba3f71ad6efffd823
kinoco-yama <kinoyama001@gmail.com>
2025-07-16 15:11:30 +0900

    チェスボードでAR描画（座標軸）

--- ConnectMQ3andOvrUDPTest/camera_checker.py
+++ ConnectMQ3andOvrUDPTest/camera_checker.py
@@ -1,7 +1,6 @@
 import cv2
-import numpy as np
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 # 解像度設定
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
@@ -11,18 +10,7 @@ while True:
     if not ret:
         break
 
-    # VideoCaptureは8bitが限界
-    # 本来の16bitを8bitに圧縮して取得（重なる）
-    # 上位8bit：左目のグレイスケール
-    # 下位8bit：右目のBayer
-    unit8 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Bayer補間でカラー化
-    color = cv2.cvtColor(unit8, cv2.COLOR_BAYER_GB2BGR)
-
-    cv2.imshow("Right Eye - Color Only", frame)
-
-    # cv2.imshow("Ovrvision Camera", frame)
+    cv2.imshow("Camera Check", frame)
     if cv2.waitKey(1) == 27:  # ESCキーで終了
         print("カメラチェックを終了します。")
         break
--- MainProject/main.py
+++ MainProject/main.py
@@ -1,17 +1,47 @@
 import cv2
+import time
 from CameraCalibrator import CameraCalibrator
 
 
 def main():
-    background_img = cv2.imread("./input.jpg")
+    # カメラ読み込み
+    cap = cv2.VideoCapture(0)
+    # 解像度設定
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+    # 動画出力設定（MP4形式）
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # ※重要
+    out = cv2.VideoWriter("output.mp4", fourcc, 30.0, (1280, 720))
+
+    # カメラキャリブレーション
     calibrator = CameraCalibrator(9, 6, 1.27, "./calib_images")
-
     calibrator.calibrate()
-    calibrator.estimate_pose(background_img)
-    ret, img = calibrator.draw_axis()
-    if ret:
-        cv2.imwrite("./output.jpg", img)
+
+    # カメラチェック
+    while True:
+        ret, frame = cap.read()
+        # 画像が取得できなかった場合は終了
+        if not ret:
+            break
+
+        # 画像を入力
+        # frame = cv2.imread("./input.jpg")
+        # 姿勢推定
+        calibrator.estimate_pose(frame)
+        # AR描画
+        ret, result_frame = calibrator.draw_axis()
+        if ret:
+            out.write(result_frame)  # 動画保存
+            cv2.imshow("Camera Check", result_frame)
+            time.sleep(0.02)
+            if cv2.waitKey(1) == 27:  # ESCキーで終了
+                print("カメラチェックを終了します。")
+                break
+    # 後片付け
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
